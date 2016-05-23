0874c6e81d1f0389f461b7be80339445597c2fc5
laurits <laurits.randers@gmail.com>
2016-05-23 22:00:44 +0200

    arg fix

--- Indexer/Indexer.py
+++ Indexer/Indexer.py
@@ -7,10 +7,9 @@ import mimetypes
 
 
 # Try and catch any wrong input in arguments
-for argument in sys.argv:
-    if argument != sys.argv[0]:
-        if not os.path.isfile(argument):
-            raise Exception("File " + argument + " does not exist")
-        if mimetypes.guess_type(argument)[0] != 'text/plain':
-            raise Exception("File " + argument + " is not a plain text file")
+for argument in sys.argv[1:]:
+    if not os.path.isfile(argument):
+        raise Exception("File " + argument + " does not exist")
+    if mimetypes.guess_type(argument)[0] != 'text/plain':
+        raise Exception("File " + argument + " is not a plain text file")
 
