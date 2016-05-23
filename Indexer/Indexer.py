a7e72f7907561859e7f911d54e8a5d4ccde5609f
Rasmus Kjær <rasmase@gmail.com>
2016-05-23 22:06:35 +0200

    Renamed extra files to single word

--- Indexer/Indexer.py
+++ Indexer/Indexer.py
@@ -12,4 +12,3 @@ for argument in sys.argv[1:]:
         raise Exception("File " + argument + " does not exist")
     if mimetypes.guess_type(argument)[0] != 'text/plain':
         raise Exception("File " + argument + " is not a plain text file")
-
