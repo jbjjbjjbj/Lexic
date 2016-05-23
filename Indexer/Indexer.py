cf6a7623ea9d187db135a3fb7a454f6ed66501c2
Rasmus Kjær <rasmase@gmail.com>
2016-05-23 21:59:37 +0200

    HOTFIX: I am an idiot

--- Indexer/Indexer.py
+++ Indexer/Indexer.py
@@ -8,8 +8,9 @@ import mimetypes
 
 # Try and catch any wrong input in arguments
 for argument in sys.argv:
-    if not os.path.isfile(argument):
-        raise Exception("File " + argument + " does not exist")
-    if mimetypes.guess_type(os.path.abspath(argument)) == 'text/plain':
-        raise Exception("File " + argument + " is not a plain text file")
+    if argument != sys.argv[0]:
+        if not os.path.isfile(argument):
+            raise Exception("File " + argument + " does not exist")
+        if mimetypes.guess_type(argument)[0] != 'text/plain':
+            raise Exception("File " + argument + " is not a plain text file")
 
