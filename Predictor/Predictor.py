8ed2e08ca38b1168834e62017ac7dc9351b42973
Rasmus Rendal <RasmusRendal@gmail.com>
2016-05-25 14:26:07 +0200

    Moved pykeylogger and modified it to work with python3. Also changed predictor a little bit

--- Predictor/Predictor.py
+++ Predictor/Predictor.py
@@ -2,9 +2,17 @@
 
 import sqlite3
 import sys
+from time import sleep, time
+import ctypes as ct
+from ctypes.util import find_library
+from pykeylogger import keylogger
+import time
 
 con = None
 cursor = None
+
+#Sorry, but I depend on X11 at the moment. 
+assert("linux" in sys.platform)
 
 def initiateDB(fileName):
     try:
@@ -18,17 +26,21 @@ def getWords(PrevWord):
     cursor.execute("SELECT * FROM Completions WHERE PrevWord=? ORDER BY Weight DESC", (PrevWord,))
     return cursor
 
-#Hold on to your butts
 initiateDB(sys.argv[1])
-while True:
-    textInput = input("Enter previous word: ")
-    words = getWords(textInput)
-    a = 0
-    while a < 10:
-        row = words.fetchone()
 
-        if row == None:
-            break
-        
-        print(row[1])
-        a+=1
+def cliLoop():
+    #Hold on to your butts
+    while True:
+        textInput = input("Enter previous word: ")
+        words = getWords(textInput)
+        a = 0
+        while a < 10:
+            row = words.fetchone()
+    
+            if row == None:
+                break
+            
+            print(row[1])
+            a+=1
+
+#cliLoop()
